c027130aff661f6857ca7b3c0c7ae9555d0240e0
Danish <danishalam861@gmail.com>
2023-01-07 16:42:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/user");
+require("./services/passport");
+
+// const authRoutes = require('./routes/authRoutes')
+
+mongoose.connect(keys.mongooseURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
- res.send({ hi: "there" });
-});
+app.use(
+ cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey],
+ })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// authRoutes(app)
+require("./routes/authRoutes")(app); // Calling authRoutes function in one line.
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
